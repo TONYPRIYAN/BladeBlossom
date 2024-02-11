f2fbc706e722d3fb001df767b236c7bca3c40fca
TONYPRIYAN <103183712+TONYPRIYAN@users.noreply.github.com>
2024-02-11 12:57:05 +0530

    chapter 1

---
+++ lib/services/assets_manager.dart
@@ -0,0 +1,22 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class AssetsManager {
+  static String imagePath = "assets/images";
+  static String userImage = "$imagePath/person.png";
+  static String botImage = "$imagePath/blade2.gif";
+  static String openaiLogo = "$imagePath/blossomblade1.jpeg";
+
+
+
+  static const Color mainFontColor = Color.fromRGBO(19, 61, 95, 1);
+  static const Color firstSuggestionBoxColor = Color(0xFF8265E5);
+  static const Color secondSuggestionBoxColor = Color(0x55434277);
+  static const Color thirdSuggestionBoxColor = Color(0xEE434277);
+  static const Color assistantCircleColor = Color.fromRGBO(209, 243, 249, 1);
+  static const Color borderColor = Color.fromRGBO(200, 200, 200, 1);
+  static const Color blackColor = Colors.black;
+  static const Color whiteColor = Colors.white;
+  static const Color mainbg = Color(0xFF191C47);
+}
