79aaa47b19e7482f3c331e116d5ada7e06a1d8a3
TONYPRIYAN <103183712+TONYPRIYAN@users.noreply.github.com>
2024-02-12 16:25:20 +0530

    update

--- lib/constants/api_consts.dart
+++ lib/constants/api_consts.dart
@@ -1,2 +1,2 @@
 String BASE_URL = "https://api.openai.com/v1";
-String API_KEY = "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";
+String API_KEY = "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";
